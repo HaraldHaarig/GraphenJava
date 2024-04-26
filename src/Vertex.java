7c9191d56bc421860a70d84c4adc613382c98b87
HaraldHaarig <132935818+HaraldHaarig@users.noreply.github.com>
2024-04-26 12:14:12 +0200

    good commit

--- src/Vertex.java
+++ src/Vertex.java
@@ -3,10 +3,12 @@ import java.util.ArrayList;
 public class Vertex {
     private int num;
     private ArrayList<Edge> edgelist;
+    private boolean searched;
 
     public Vertex(int num) {
         this.num = num;
         edgelist = new ArrayList<Edge>();
+        searched = false;
     }
 
     public int getNum() {
